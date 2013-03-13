ced3a2109c0f8949e8fd5849d8a251f642d977c5
Gonzalo Parajon <gonzalo.parajon@gmail.com>
2013-03-12 19:09:29 -0700

    test

---
+++ MemoryCapsule/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  MemoryCapsule
+//
+//  Created by Gonzalo Parajon on 3/12/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end

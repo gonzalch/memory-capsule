563b1176593aae158fe651565c62e823d862ae4b
Gonzalo Parajon <gonzalo.parajon@gmail.com>
2013-03-17 23:28:54 -0700

    Finished working on the HomeView & HomeView navigation controllers

---
+++ MemoryCapsule/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  MemoryCapsule
+//
+//  Created by Gonzalo Parajon on 3/17/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface HomeViewController : ViewController
+
+@end

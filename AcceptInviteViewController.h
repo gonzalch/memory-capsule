81bd3bde087e46f71c34ec21d1f60965d05702e0
Gonzalo Parajon <gonzalo.parajon@gmail.com>
2013-03-26 17:14:41 -0700

    Started working on accept friends invite

---
+++ AcceptInviteViewController.h
@@ -0,0 +1,13 @@
+//
+//  AcceptInviteViewController.h
+//  MemoryCapsule
+//
+//  Created by Gonzalo Parajon on 3/26/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AcceptInviteViewController : ViewController
+
+@end
---
+++ AcceptInviteViewController.m
@@ -0,0 +1,38 @@
+//
+//  AcceptInviteViewController.m
+//  MemoryCapsule
+//
+//  Created by Gonzalo Parajon on 3/26/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "AcceptInviteViewController.h"
+
+@interface AcceptInviteViewController ()
+
+@end
+
+@implementation AcceptInviteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

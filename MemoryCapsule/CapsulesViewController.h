f2e1e657f478ab5158432d273a6419c803f0574a
Chris Gonzalez <chrisg533@gmail.com>
2013-03-26 17:11:41 -0700

    added CapsulesViewController

---
+++ MemoryCapsule/CapsulesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CapsulesViewController.h
+//  MemoryCapsule
+//
+//  Created by Chris Gonzalez on 3/26/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface CapsulesViewController : ViewController
+
+@end
---
+++ MemoryCapsule/CapsulesViewController.m
@@ -0,0 +1,38 @@
+//
+//  CapsulesViewController.m
+//  MemoryCapsule
+//
+//  Created by Chris Gonzalez on 3/26/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "CapsulesViewController.h"
+
+@interface CapsulesViewController ()
+
+@end
+
+@implementation CapsulesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

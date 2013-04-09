e7f53036b7ebe1bb834a7d0c8def9cbbba7f8db5
Christopher Gonzalez <chrisg533@gmail.com>
2013-04-09 16:20:08 -0700

    invite friends to capsule

---
+++ InviteFriendsToCapsuleViewController.h
@@ -0,0 +1,13 @@
+//
+//  InviteFriendsToCapsuleViewController.h
+//  MemoryCapsule
+//
+//  Created by Chris Gonzalez on 4/9/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface InviteFriendsToCapsuleViewController : ViewController
+
+@end
---
+++ InviteFriendsToCapsuleViewController.m
@@ -0,0 +1,38 @@
+//
+//  InviteFriendsToCapsuleViewController.m
+//  MemoryCapsule
+//
+//  Created by Chris Gonzalez on 4/9/13.
+//  Copyright (c) 2013 teambrown. All rights reserved.
+//
+
+#import "InviteFriendsToCapsuleViewController.h"
+
+@interface InviteFriendsToCapsuleViewController ()
+
+@end
+
+@implementation InviteFriendsToCapsuleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
